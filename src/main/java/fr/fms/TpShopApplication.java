7a85dd78430f53b3bde087959a6127ee738e798c
NathPascal <nath.pascal03@gmail.com>
2024-12-11 13:46:26 +0100

    création des fichiers

---
+++ src/main/java/fr/fms/TpShopApplication.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpShopApplication.class, args);
+	}
+
+}
